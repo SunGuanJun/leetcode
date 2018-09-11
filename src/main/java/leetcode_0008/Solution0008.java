142fa828eaff855b4a121f13bed8e781a2550ac0
SunGuanJun <1532945407@qq.com>
2018-09-11 19:16:17 +0800

    add:字符串转数字

--- src/main/java/leetcode_0008/Solution0008.java
+++ src/main/java/leetcode_0008/Solution0008.java
@@ -5,6 +5,54 @@ package leetcode_0008;
  */
 public class Solution0008 {
     public int myAtoi(String str) {
-        return 0;
+
+        int i;
+        int res = 0;
+        for (i=0; i<str.length(); i++){
+            if (str.charAt(i) != ' '){
+                break;
+            }
+        }
+
+        boolean posFlag = true;
+        if (i == str.length()){
+            return 0;
+        }
+        else if (str.charAt(i) == '-'){
+            posFlag = false;
+            i++;
+        }
+        else if (str.charAt(i) == '+'){
+            i++;
+        }
+        else if (str.charAt(i) < '0' || str.charAt(i) > '9'){
+            return 0;
+        }
+
+        int max = posFlag ? Integer.MAX_VALUE / 10 : 0 - Integer.MIN_VALUE / 10;
+        while (i<str.length() && str.charAt(i) >= '0' && str.charAt(i) <= '9'){
+            if (res < max){
+                res = res * 10 + str.charAt(i) - '0';
+            }
+            else if (res == max){
+                if (posFlag && str.charAt(i) > '7'){
+                    return Integer.MAX_VALUE;
+                }
+                else if (!posFlag && str.charAt(i) > '8'){
+                    return Integer.MIN_VALUE;
+                }
+            }
+            else {
+                return posFlag ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            }
+            i++;
+        }
+
+        return posFlag ? res : -res;
+    }
+
+    public static void main(String[] args){
+        System.out.println(Character.isSpaceChar(' '));
+        System.out.println(Character.isSpaceChar('\t'));
     }
 }
