f85e6c609fbae75176c49f436587367fbb6486bb
SunGuanJun <1532945407@qq.com>
2018-09-11 19:31:55 +0800

    add:回文数

--- src/main/java/leetcode_0009/Solution0009.java
+++ src/main/java/leetcode_0009/Solution0009.java
@@ -5,6 +5,23 @@ package leetcode_0009;
  */
 public class Solution0009 {
     public boolean isPalindrome(int x) {
-        return true;
+        if (x < 0){
+            return false;
+        }
+        else if (x == 0) {
+            return true;
+        }
+        else {
+            return x == reverse(x);
+        }
+    }
+
+    public int reverse(int x){
+        int y = 0;
+        while (x != 0){
+            y = y * 10 + x % 10;
+            x /= 10;
+        }
+        return y;
     }
 }
