e92b55d533bd7d83cb63479dfd174e074401e733
sunguanjun <1532945407@qq.com>
2018-09-13 23:42:17 +0800

    add:有效的括号“

--- src/main/java/leetcode_0001_0050/leetcode_0020/Solution0020.java
+++ src/main/java/leetcode_0001_0050/leetcode_0020/Solution0020.java
@@ -1,10 +1,59 @@
 package leetcode_0001_0050.leetcode_0020;
 
 /**
+ * (){}[]
  * Created by sun on 2018/9/13.
  */
 public class Solution0020 {
     public boolean isValid(String s) {
-        return true;
+        char[] chars = new char[s.length()];
+        int index=0;
+
+        for (int i=0; i<s.length(); i++){
+            switch (s.charAt(i)){
+                case '(':
+                case '{':
+                case '[':
+                    chars[index] = s.charAt(i);
+                    index++;
+                    break;
+                case ')':
+                    if (index > 0){
+                        index--;
+                    }
+                    else {
+                        return false;
+                    }
+                    if (chars[index] != '('){
+                        return false;
+                    }
+                    break;
+                case '}':
+                    if (index > 0){
+                        index--;
+                    }
+                    else {
+                        return false;
+                    }
+                    if (chars[index] != '{'){
+                        return false;
+                    }
+                    break;
+                case ']':
+                    if (index > 0){
+                        index--;
+                    }
+                    else {
+                        return false;
+                    }
+                    if (chars[index] != '['){
+                        return false;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+        return index > 0 ? false : true;
     }
 }
