3bcef0f6c9c5e2f6ed1813b315da6dda30a7a989
sunguanjun <1532945407@qq.com>
2018-09-14 00:28:48 +0800

    add:题目

--- src/main/java/leetcode_0001_0050/leetcode_0022/Solution0022.java
+++ src/main/java/leetcode_0001_0050/leetcode_0022/Solution0022.java
@@ -1,5 +1,9 @@
 package leetcode_0001_0050.leetcode_0022;
 
-public class Solution0022 {
+import java.util.List;
 
+public class Solution0022 {
+    public List<String> generateParenthesis(int n) {
+        return null;
+    }
 }
--- src/main/java/leetcode_0001_0050/leetcode_0024/Solution0024.java
+++ src/main/java/leetcode_0001_0050/leetcode_0024/Solution0024.java
@@ -1,5 +1,13 @@
 package leetcode_0001_0050.leetcode_0024;
 
 public class Solution0024 {
+    public ListNode swapPairs(ListNode head) {
+        return null;
+    }
+}
 
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
 }
