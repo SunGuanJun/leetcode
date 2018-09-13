3a0c276d2a227c23965aa06f38f935805857d5b8
sunguanjun <1532945407@qq.com>
2018-09-14 00:28:48 +0800

    add:题目

--- src/main/java/leetcode_0001_0050/leetcode_0023/Solution0023.java
+++ src/main/java/leetcode_0001_0050/leetcode_0023/Solution0023.java
@@ -1,5 +1,13 @@
 package leetcode_0001_0050.leetcode_0023;
 
 public class Solution0023 {
+    public ListNode mergeKLists(ListNode[] lists) {
+        return null;
+    }
+}
 
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
 }
