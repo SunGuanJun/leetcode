54508cbaaf3b4a639da0e787c08860d8140f8c43
sunguanjun <1532945407@qq.com>
2018-09-13 23:03:46 +0800

    add:删除链表倒数第n个数“

--- src/main/java/leetcode_0001_0050/leetcode_0019/Solution0019.java
+++ src/main/java/leetcode_0001_0050/leetcode_0019/Solution0019.java
@@ -6,8 +6,20 @@ package leetcode_0001_0050.leetcode_0019;
  */
 public class Solution0019 {
     public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode n1=head, n2=head;
+        for (int i=0; i<n; i++){
+            n1 = n1.next;
+        }
 
-        return null;
+        if (n1 == null){
+            return head.next;
+        }
+        while (n1.next != null){
+            n1 = n1.next;
+            n2 = n2.next;
+        }
+        n2.next = n2.next.next;
+        return head;
     }
 }
 
