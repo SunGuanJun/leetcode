3261c251dc5b3646e713d7947afe8e329829245f
sunguanjun <1532945407@qq.com>
2018-09-13 23:03:46 +0800

    add:删除链表倒数第n个数“

--- src/test/java/leetcode_0001_0050/leetcode_0017/Solution0017Test.java
+++ src/test/java/leetcode_0001_0050/leetcode_0017/Solution0017Test.java
@@ -1,12 +1,17 @@
 package leetcode_0001_0050.leetcode_0017;
 
+import com.alibaba.fastjson.JSON;
 import org.junit.Test;
 
+/**
+ * 输入："23"
+ 输出：["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+ */
 public class Solution0017Test {
 	Solution0017 solution0017 = new Solution0017();
 
 	@Test
 	public void test1(){
-
+		System.out.println(JSON.toJSONString(solution0017.letterCombinations("9387")));
 	}
 }
