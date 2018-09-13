9f77c4cac42ddbde017c117074fd080ca1130261
sunguanjun <1532945407@qq.com>
2018-09-14 00:28:48 +0800

    add:题目

--- src/main/java/leetcode_0001_0050/leetcode_0021/Solution0021.java
+++ src/main/java/leetcode_0001_0050/leetcode_0021/Solution0021.java
@@ -1,5 +1,14 @@
 package leetcode_0001_0050.leetcode_0021;
 
-public class Solution0021 {
 
+public class Solution0021 {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        return null;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
 }
--- src/main/java/leetcode_0001_0050/leetcode_0025/Solution0025.java
+++ src/main/java/leetcode_0001_0050/leetcode_0025/Solution0025.java
@@ -1,5 +1,13 @@
 package leetcode_0001_0050.leetcode_0025;
 
 public class Solution0025 {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        return null;
+    }
+}
 
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
 }
