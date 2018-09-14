7df3d6b3a193f613cd6e307b98d8c7a246417bbb
sunguanjun <1532945407@qq.com>
2018-09-15 01:09:30 +0800

    add:微调“

--- src/test/java/leetcode_0001_0050/leetcode_0018/Solution0018Test.java
+++ src/test/java/leetcode_0001_0050/leetcode_0018/Solution0018Test.java
@@ -3,6 +3,8 @@ package leetcode_0001_0050.leetcode_0018;
 import com.alibaba.fastjson.JSON;
 import org.junit.Test;
 
+import java.util.List;
+
 public class Solution0018Test {
 	Solution0018 solution0018 = new Solution0018();
 
@@ -10,5 +12,13 @@ public class Solution0018Test {
 	public void test1(){
 		int[] nums = {1, 0, -1, 0, -2, 2};
 		System.out.println(JSON.toJSONString(solution0018.fourSum(nums, 0)));
+	}
+
+	@Test
+	public void test2(){
+		int[] nums = {-3,-2,-1,0,0,1,2,3};
+		List list = solution0018.fourSum(nums, 0);
+		System.out.println(JSON.toJSONString(list));
+		System.out.println(list.size());
 	}
 }
