cefa5fa7ba6c76e024e6c86910fc9ed734d4bc6c
sunguanjun <hzsunguanjun.corp.netease.com>
2018-09-14 20:37:36 +0800

    add:两两交换链表节点

--- src/main/java/leetcode_0001_0050/leetcode_0024/Solution0024.java
+++ src/main/java/leetcode_0001_0050/leetcode_0024/Solution0024.java
@@ -1,8 +1,34 @@
 package leetcode_0001_0050.leetcode_0024;
 
+/**
+ * 给定 1->2->3->4, 你应该返回 2->1->4->3.
+ */
 public class Solution0024 {
     public ListNode swapPairs(ListNode head) {
-        return null;
+        ListNode n1=null, n2=null, current=head,pre=null;
+        while (current != null){
+            if (n1 == null){
+                n1 = current;
+                current = current.next;
+            }
+            else {
+                n2 = current;
+                if (n1 == head){
+                    head = n2;
+                }
+                current = current.next;
+
+                n1.next = n2.next;
+                n2.next = n1;
+                if (pre !=null){
+                    pre.next = n2;
+                }
+                pre = n1;
+                n1 =null;
+            }
+        }
+
+        return head;
     }
 }
 
--- src/test/java/leetcode_0001_0050/leetcode_0024/Solution0024Test.java
+++ src/test/java/leetcode_0001_0050/leetcode_0024/Solution0024Test.java
@@ -7,6 +7,22 @@ public class Solution0024Test {
 
 	@Test
 	public void test1(){
+		ListNode l1 = new ListNode(1);
+		ListNode l2 = new ListNode(2);
+		ListNode l3 = new ListNode(3);
+		ListNode l4 = new ListNode(4);
+		ListNode l13 = new ListNode(13);
+		ListNode l14 = new ListNode(14);
+		l1.next = l2;
+		l2.next = l3;
+		l3.next = l4;
+		l4.next = l13;
+		l13.next = l14;
 
+		ListNode l = solution0024.swapPairs(l1);
+		while (l != null){
+			System.out.println(l.val);
+			l = l.next;
+		}
 	}
 }
