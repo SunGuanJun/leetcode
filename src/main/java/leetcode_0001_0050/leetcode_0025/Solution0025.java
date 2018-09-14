6c228b479130cfa9748642fb692f22f2a9bb0b2d
SunGuanJun <1532945407@qq.com>
2018-09-14 20:40:31 +0800

    add:k翻转

--- src/main/java/leetcode_0001_0050/leetcode_0025/Solution0025.java
+++ src/main/java/leetcode_0001_0050/leetcode_0025/Solution0025.java
@@ -1,5 +1,12 @@
 package leetcode_0001_0050.leetcode_0025;
 
+/**
+ * 给定这个链表：1->2->3->4->5
+ *
+ *     当 k = 2 时，应当返回: 2->1->4->3->5
+ *
+ *     当 k = 3 时，应当返回: 3->2->1->4->5
+ */
 public class Solution0025 {
     public ListNode reverseKGroup(ListNode head, int k) {
         return null;
