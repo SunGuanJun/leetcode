a2e1350a0e8242a67ae01d602a3c57995af80128
sunguanjun <hzsunguanjun.corp.netease.com>
2018-09-14 09:40:20 +0800

    add:四数之和

--- src/main/java/leetcode_0001_0050/leetcode_0018/Solution0018.java
+++ src/main/java/leetcode_0001_0050/leetcode_0018/Solution0018.java
@@ -1,5 +1,7 @@
 package leetcode_0001_0050.leetcode_0018;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -7,7 +9,37 @@ import java.util.List;
  */
 public class Solution0018 {
     public List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
 
-        return null;
+        List<List<Integer>> results = new ArrayList<>();
+        for (int i1=0; i1<nums.length-3; i1++){
+            for (int i2=i1+1; i2<nums.length-2; i2++){
+                int i3=i2+1, i4=nums.length-1;
+                while (i3<i4){
+                    int sum = nums[i1] + nums[i2] + nums[i3] + nums[i4];
+                    if (sum == target){
+                        List<Integer> result = new ArrayList<>();
+                        result.add(nums[i1]);
+                        result.add(nums[i2]);
+                        result.add(nums[i3]);
+                        result.add(nums[i4]);
+                        results.add(result);
+                        do {
+                            i3++;
+                        } while (i3<nums.length-1 && nums[i3]== nums[i3-1]);
+                        do {
+                            i4--;
+                        } while (i4>0 && nums[i4]==nums[i4+1]);
+                    }
+                    else if (sum < target){
+                        i3++;
+                    }
+                    else {
+                        i4--;
+                    }
+                }
+            }
+        }
+        return results;
     }
 }
