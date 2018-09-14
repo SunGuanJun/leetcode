13efde17cfa4e9b3d8919ee0f7b6f1c7a09e0500
SunGuanJun <1532945407@qq.com>
2018-09-14 20:05:50 +0800

    add:合并k个排序链表

--- src/main/java/leetcode_0001_0050/leetcode_0023/Solution0023.java
+++ src/main/java/leetcode_0001_0050/leetcode_0023/Solution0023.java
@@ -1,8 +1,43 @@
 package leetcode_0001_0050.leetcode_0023;
 
+/**
+ *  输入:
+ *     [
+ *       1->4->5,
+ *       1->3->4,
+ *       2->6
+ *     ]
+ *     输出: 1->1->2->3->4->4->5->6
+ */
 public class Solution0023 {
     public ListNode mergeKLists(ListNode[] lists) {
-        return null;
+        ListNode l=null, li = new ListNode(0), tmp;
+        int index=0;
+
+        while (true){
+            tmp = null;
+            for (int i=0; i<lists.length; i++){
+                if (lists[i] != null){
+                    if (tmp == null || lists[i].val < tmp.val){
+                        tmp = lists[i];
+                        index = i;
+                    }
+                }
+            }
+            if (tmp == null){
+                break;
+            }
+            else {
+                li.next = tmp;
+                li = li.next;
+                tmp = tmp.next;
+                lists[index] = tmp;
+                if (l == null){
+                    l = li;
+                }
+            }
+        }
+        return l;
     }
 }
 
--- src/test/java/leetcode_0001_0050/leetcode_0023/Solution0023Test.java
+++ src/test/java/leetcode_0001_0050/leetcode_0023/Solution0023Test.java
@@ -7,6 +7,31 @@ public class Solution0023Test {
 
 	@Test
 	public void test1(){
+		ListNode l1 = new ListNode(1);
+		ListNode l2 = new ListNode(2);
+		ListNode l3 = new ListNode(4);
+		l1.next = l2;
+		l2.next = l3;
 
+		ListNode l4 = new ListNode(1);
+		ListNode l5 = new ListNode(3);
+		ListNode l6 = new ListNode(4);
+		l4.next = l5;
+		l5.next = l6;
+
+		ListNode l7 = new ListNode(2);
+		ListNode l8 = new ListNode(5);
+		ListNode l9 = new ListNode(8);
+		l7.next = l8;
+		l8.next = l9;
+
+		ListNode[] listNodes = {l1, l4, l7};
+
+		ListNode l = solution0023.mergeKLists(listNodes);
+
+		while (l != null){
+			System.out.println(l.val);
+			l = l.next;
+		}
 	}
 }
