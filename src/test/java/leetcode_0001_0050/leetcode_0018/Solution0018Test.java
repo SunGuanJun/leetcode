156aa25891fc0ebc7859b5aa7d093f84a87a2299
sunguanjun <hzsunguanjun.corp.netease.com>
2018-09-14 09:40:20 +0800

    add:四数之和

--- src/test/java/leetcode_0001_0050/leetcode_0018/Solution0018Test.java
+++ src/test/java/leetcode_0001_0050/leetcode_0018/Solution0018Test.java
@@ -1,5 +1,6 @@
 package leetcode_0001_0050.leetcode_0018;
 
+import com.alibaba.fastjson.JSON;
 import org.junit.Test;
 
 public class Solution0018Test {
@@ -7,6 +8,7 @@ public class Solution0018Test {
 
 	@Test
 	public void test1(){
-
+		int[] nums = {1, 0, -1, 0, -2, 2};
+		System.out.println(JSON.toJSONString(solution0018.fourSum(nums, 0)));
 	}
 }
