68a65e37ecb47b4f5cc9f61851f0a7361c0279a9
SunGuanJun <1532945407@qq.com>
2018-09-14 19:33:03 +0800

    add:合并两个有序链表

--- src/main/java/leetcode_0001_0050/leetcode_0021/Solution0021.java
+++ src/main/java/leetcode_0001_0050/leetcode_0021/Solution0021.java
@@ -3,7 +3,31 @@ package leetcode_0001_0050.leetcode_0021;
 
 public class Solution0021 {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        return null;
+        ListNode l3=null, l3i= new ListNode(0);
+
+        while (l1 != null && l2 != null){
+            if (l1.val < l2.val){
+                l3i.next = l1;
+                l1 = l1.next;
+            }
+            else {
+                l3i.next = l2;
+                l2 = l2.next;
+            }
+            l3i = l3i.next;
+            l3i.next = null;
+            if (l3 == null){
+                l3 = l3i;
+            }
+        }
+        if (l1 != null){
+            l3i.next = l1;
+        }
+        if (l2 != null){
+            l3i.next = l2;
+        }
+
+        return l3;
     }
 }
 
