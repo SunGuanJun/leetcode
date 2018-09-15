078eae53112594f95d9c5127e7a8066db032a39e
sunguanjun <hzsunguanjun.corp.netease.com>
2018-09-15 22:40:39 +0800

    add:移除元素

--- src/test/java/leetcode_0001_0050/leetcode_0027/Solution0027Test.java
+++ src/test/java/leetcode_0001_0050/leetcode_0027/Solution0027Test.java
@@ -1,5 +1,6 @@
 package leetcode_0001_0050.leetcode_0027;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class Solution0027Test {
@@ -7,6 +8,48 @@ public class Solution0027Test {
 
 	@Test
 	public void test1(){
+//		int[] nums = {0,1,2,2,3,0,4,2};
+		int[] nums = {3,2,2,3};
+//		int[] nums = {1};
 
+		// nums 是以“引用”方式传递的。也就是说，不对实参做任何拷贝
+		int len = solution0027.removeElement(nums, 3);
+
+		// 在函数里修改输入数组对于调用者是可见的。
+		// 根据你的函数返回的长度, 它会打印出数组中该长度范围内的所有元素。
+		for (int i = 0; i < len; i++) {
+			System.out.println(nums[i]);
+		}
+		Assert.assertEquals(2, len);
+	}
+
+	@Test
+	public void test2(){
+		int[] nums = {0,1,2,2,3,0,4,2};
+
+		// nums 是以“引用”方式传递的。也就是说，不对实参做任何拷贝
+		int len = solution0027.removeElement(nums, 2);
+
+		// 在函数里修改输入数组对于调用者是可见的。
+		// 根据你的函数返回的长度, 它会打印出数组中该长度范围内的所有元素。
+		for (int i = 0; i < len; i++) {
+			System.out.println(nums[i]);
+		}
+		Assert.assertEquals(5, len);
+	}
+
+	@Test
+	public void test3(){
+		int[] nums = {1};
+
+		// nums 是以“引用”方式传递的。也就是说，不对实参做任何拷贝
+		int len = solution0027.removeElement(nums, 1);
+
+		// 在函数里修改输入数组对于调用者是可见的。
+		// 根据你的函数返回的长度, 它会打印出数组中该长度范围内的所有元素。
+		for (int i = 0; i < len; i++) {
+			System.out.println(nums[i]);
+		}
+		Assert.assertEquals(0, len);
 	}
 }
