5476b68ce5e69c33db2f989ae824a8a7257d8cef
SunGuanJun <1532945407@qq.com>
2018-09-15 22:40:39 +0800

    add:移除元素

--- src/main/java/leetcode_0001_0050/leetcode_0027/Solution0027.java
+++ src/main/java/leetcode_0001_0050/leetcode_0027/Solution0027.java
@@ -1,5 +1,19 @@
 package leetcode_0001_0050.leetcode_0027;
 
 public class Solution0027 {
-
+    public int removeElement(int[] nums, int val) {
+        int len = 0, i=0;
+        while (i<nums.length){
+            while (i<nums.length && nums[i] == val){
+                i++;
+            }
+            if (i==nums.length){
+                break;
+            }
+            nums[len] = nums[i];
+            len++;
+            i++;
+        }
+        return len;
+    }
 }
