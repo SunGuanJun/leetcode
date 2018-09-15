1247cf6cb422307509593123d9bb2c57ddcb60b6
sunguanjun <hzsunguanjun.corp.netease.com>
2018-09-15 23:29:21 +0800

    add:实现strStr

--- src/main/java/leetcode_0001_0050/leetcode_0028/Solution0028.java
+++ src/main/java/leetcode_0001_0050/leetcode_0028/Solution0028.java
@@ -1,5 +1,33 @@
 package leetcode_0001_0050.leetcode_0028;
 
 public class Solution0028 {
+    public int strStr(String haystack, String needle) {
+        int l1=haystack.length();
+        int l2=needle.length();
 
+        if (l2 == 0){
+            return 0;
+        }
+        if(l2>l1){
+            return -1;
+        }
+
+        char[] chars1 = haystack.toCharArray();
+        char[] chars2 = needle.toCharArray();
+        for (int i=0; i<l1; i++){
+            if (chars1[i] == chars2[0]){
+                int i1=i+1;
+                int j;
+                for (j=1;j<l2 && i1<l1; i1++,j++){
+                    if (chars1[i1] != chars2[j]){
+                        break;
+                    }
+                }
+                if (j == l2){
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
 }
--- src/test/java/leetcode_0001_0050/leetcode_0028/Solution0028Test.java
+++ src/test/java/leetcode_0001_0050/leetcode_0028/Solution0028Test.java
@@ -1,5 +1,6 @@
 package leetcode_0001_0050.leetcode_0028;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class Solution0028Test {
@@ -7,6 +8,21 @@ public class Solution0028Test {
 
 	@Test
 	public void test1(){
+		Assert.assertEquals(2, solution0028.strStr("hello", "ll"));
+	}
 
+	@Test
+	public void test2(){
+		Assert.assertEquals(-1, solution0028.strStr("aaaaa", "bba"));
+	}
+
+	@Test
+	public void test3(){
+		Assert.assertEquals(0, solution0028.strStr("aaaaa", ""));
+	}
+
+	@Test
+	public void test4(){
+		Assert.assertEquals(1, solution0028.strStr("mississippi", "issi"));
 	}
 }
