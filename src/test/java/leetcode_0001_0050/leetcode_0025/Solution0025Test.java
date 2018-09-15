c391afe40e8829d42cbcc3973d011ca51c57ca6d
sunguanjun <hzsunguanjun.corp.netease.com>
2018-09-15 14:38:35 +0800

    add:k组翻转

--- src/test/java/leetcode_0001_0050/leetcode_0025/Solution0025Test.java
+++ src/test/java/leetcode_0001_0050/leetcode_0025/Solution0025Test.java
@@ -7,6 +7,43 @@ public class Solution0025Test {
 
 	@Test
 	public void test1(){
+		ListNode l1 = new ListNode(1);
+		ListNode l2 = new ListNode(2);
+		ListNode l3 = new ListNode(3);
+		ListNode l4 = new ListNode(4);
+		ListNode l13 = new ListNode(13);
+		ListNode l14 = new ListNode(14);
+		l1.next = l2;
+		l2.next = l3;
+		l3.next = l4;
+		l4.next = l13;
+		l13.next = l14;
 
+		ListNode head = solution0025.reverseKGroup(l1, 8);
+		while (head != null){
+			System.out.println(head.val);
+			head = head.next;
+		}
+	}
+
+	@Test
+	public void test2(){
+		ListNode l1 = new ListNode(1);
+		ListNode l2 = new ListNode(2);
+		ListNode l3 = new ListNode(3);
+		ListNode l4 = new ListNode(4);
+		ListNode l13 = new ListNode(13);
+		ListNode l14 = new ListNode(14);
+		l1.next = l2;
+		l2.next = l3;
+		l3.next = l4;
+		l4.next = l13;
+		l13.next = l14;
+
+		ListNode head = solution0025.reverse(l1, 2);
+		while (head != null){
+			System.out.println(head.val);
+			head = head.next;
+		}
 	}
 }
