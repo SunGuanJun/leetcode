2f8cdd354a015842502d5940aa1a5e29955c0a1b
SunGuanJun <1532945407@qq.com>
2018-09-16 09:59:50 +0800

    add:两数相除

--- src/main/java/leetcode_0001_0050/leetcode_0029/Solution0029.java
+++ src/main/java/leetcode_0001_0050/leetcode_0029/Solution0029.java
@@ -1,5 +1,10 @@
 package leetcode_0001_0050.leetcode_0029;
 
 public class Solution0029 {
-
+    public int divide(int dividend, int divisor) {
+        if (dividend == Integer.MIN_VALUE && divisor == -1){
+            return Integer.MAX_VALUE;
+        }
+        return dividend/divisor;
+    }
 }
