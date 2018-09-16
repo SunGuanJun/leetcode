645b8022e46cbbc807376131aaca79167951210e
sunguanjun <hzsunguanjun.corp.netease.com>
2018-09-16 09:59:50 +0800

    add:两数相除

--- src/test/java/leetcode_0001_0050/leetcode_0029/Solution0029Test.java
+++ src/test/java/leetcode_0001_0050/leetcode_0029/Solution0029Test.java
@@ -1,5 +1,6 @@
 package leetcode_0001_0050.leetcode_0029;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class Solution0029Test {
@@ -7,6 +8,14 @@ public class Solution0029Test {
 
 	@Test
 	public void test1(){
-
+		Assert.assertEquals(3, solution0029.divide(10, 3));
+		Assert.assertEquals(-2, solution0029.divide(7, -3));
+		Assert.assertEquals(2147483647, solution0029.divide(-2147483648, -1));
 	}
+
+	@Test
+	public void test2(){
+		Assert.assertEquals(Integer.MAX_VALUE, solution0029.divide(Integer.MIN_VALUE, -1));
+	}
+
 }
