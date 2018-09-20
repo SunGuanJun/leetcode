97f71f6f3c832aa3100f7c30bf02e6fa8de97fb0
SunGuanJun <1532945407@qq.com>
2018-09-20 17:13:54 +0800

    add:最长的有效括号

--- src/test/java/leetcode_0001_0050/leetcode_0032/Solution0032Test.java
+++ src/test/java/leetcode_0001_0050/leetcode_0032/Solution0032Test.java
@@ -1,12 +1,35 @@
 package leetcode_0001_0050.leetcode_0032;
 
+import org.junit.Assert;
 import org.junit.Test;
 
+/**
+ * 示例 1:
+ *
+ *     输入: "(()"
+ *     输出: 2
+ *     解释: 最长有效括号子串为 "()"
+ * 示例 2:
+ *
+ *     输入: ")()())"
+ *     输出: 4
+ *     解释: 最长有效括号子串为 "()()"
+ */
 public class Solution0032Test {
 	Solution0032 solution0032 = new Solution0032();
 
 	@Test
 	public void test1(){
+		Assert.assertEquals(2, solution0032.longestValidParentheses("(()"));
+	}
 
+	@Test
+	public void test2(){
+		Assert.assertEquals(4, solution0032.longestValidParentheses(")()())"));
+	}
+
+	@Test
+	public void test3(){
+		Assert.assertEquals(6, solution0032.longestValidParentheses("(((())()"));
 	}
 }
