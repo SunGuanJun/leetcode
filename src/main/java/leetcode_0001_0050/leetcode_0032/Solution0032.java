1241e8892de7b712d5bb1a910e7aa4ed25a4e0c7
sunguanjun <hzsunguanjun.corp.netease.com>
2018-09-20 17:13:54 +0800

    add:最长的有效括号

--- src/main/java/leetcode_0001_0050/leetcode_0032/Solution0032.java
+++ src/main/java/leetcode_0001_0050/leetcode_0032/Solution0032.java
@@ -1,5 +1,38 @@
 package leetcode_0001_0050.leetcode_0032;
 
-public class Solution0032 {
+import java.util.Stack;
 
+public class Solution0032 {
+    public int longestValidParentheses(String s) {
+        int[] flags = new int[s.length()];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i=0; i<s.length(); i++){
+            if (s.charAt(i) == '('){
+                flags[i] = -1;
+                stack.push(i);
+            }
+            else {
+                if (stack.empty()){
+                    flags[i] = -1;
+                }
+                else {
+                    flags[i] = stack.pop();
+                    flags[flags[i]] = i;
+                }
+            }
+        }
+
+        int max = 0, currLen=0;
+        for (int i=0; i<s.length(); i++){
+            if (flags[i] >= 0){
+                currLen++;
+                max = max > currLen ? max : currLen;
+            }
+            else {
+                currLen = 0;
+            }
+        }
+        return max;
+    }
 }
