42ab94601711dbdc4d91f0e890a8a5f709887b9d
sunguanjun <hzsunguanjun.corp.netease.com>
2018-09-20 20:45:58 +0800

    add:xx

--- src/main/java/leetcode_0001_0050/leetcode_0033/Solution0033.java
+++ src/main/java/leetcode_0001_0050/leetcode_0033/Solution0033.java
@@ -1,5 +1,29 @@
 package leetcode_0001_0050.leetcode_0033;
 
 public class Solution0033 {
+    public int search(int[] nums, int target) {
+        search(nums, target, 0, nums.length);
+        return 0;
+    }
 
+    public int search(int[] nums, int target, int left, int right){
+        if (left == right ){
+            return nums[left] == target ? left : -1;
+        }
+        else {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target){
+                return mid;
+            }
+            else {
+                if (target >= nums[left]){
+                    return search(nums, target, left, mid-1);
+                }
+                if (target <= nums[right]){
+                    return search(nums, target, mid+1, right);
+                }
+                return -1;
+            }
+        }
+    }
 }
--- src/test/java/leetcode_0001_0050/leetcode_0033/Solution0033Test.java
+++ src/test/java/leetcode_0001_0050/leetcode_0033/Solution0033Test.java
@@ -1,12 +1,30 @@
 package leetcode_0001_0050.leetcode_0033;
 
+import org.junit.Assert;
 import org.junit.Test;
 
+/**
+ * 示例 1:
+ *
+ *     输入: nums = [4,5,6,7,0,1,2], target = 0
+ *     输出: 4
+ * 示例 2:
+ *
+ *     输入: nums = [4,5,6,7,0,1,2], target = 3
+ *     输出: -1
+ */
 public class Solution0033Test {
 	Solution0033 solution0033 = new Solution0033();
 
 	@Test
 	public void test1(){
+		int[] nums = {4,5,6,7,0,1,2};
+		Assert.assertEquals(4, solution0033.search(nums, 0));
+	}
 
+	@Test
+	public void test2(){
+		int[] nums = {4,5,6,7,0,1,2};
+		Assert.assertEquals(-1, solution0033.search(nums, 3));
 	}
 }
