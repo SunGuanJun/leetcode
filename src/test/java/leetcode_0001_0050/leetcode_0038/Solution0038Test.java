4ae1dd92f41a7ecd95172d2d6867b946e9b3d5bb
sunguanjun <hzsunguanjun.corp.netease.com>
2018-09-22 21:29:00 +0800

    add:报数

--- src/test/java/leetcode_0001_0050/leetcode_0038/Solution0038Test.java
+++ src/test/java/leetcode_0001_0050/leetcode_0038/Solution0038Test.java
@@ -1,12 +1,28 @@
 package leetcode_0001_0050.leetcode_0038;
 
+import org.junit.Assert;
 import org.junit.Test;
 
+/**
+ * 示例 1:
+ *
+ * 输入: 1
+ * 输出: "1"
+ * 示例 2:
+ *
+ * 输入: 4
+ * 输出: "1211"
+ */
 public class Solution0038Test {
 	Solution0038 solution0038 = new Solution0038();
 
 	@Test
 	public void test1(){
+		Assert.assertEquals("1", solution0038.countAndSay(1));
+	}
 
+	@Test
+	public void test2(){
+		Assert.assertEquals("1211", solution0038.countAndSay(4));
 	}
 }
