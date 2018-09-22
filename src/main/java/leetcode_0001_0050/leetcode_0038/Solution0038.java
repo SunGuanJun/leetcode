d5c04dd548daf1c8ecc3eb4f1b668d93d3fc02a5
SunGuanJun <1532945407@qq.com>
2018-09-22 21:29:00 +0800

    add:报数

--- src/main/java/leetcode_0001_0050/leetcode_0038/Solution0038.java
+++ src/main/java/leetcode_0001_0050/leetcode_0038/Solution0038.java
@@ -1,5 +1,27 @@
 package leetcode_0001_0050.leetcode_0038;
 
 public class Solution0038 {
+    public String countAndSay(int n) {
+        String s = "1";
+        for (int i=1; i<n; i++){
+            s = countAndSay(s);
+        }
+        return s;
+    }
 
+    public String countAndSay(String s1) {
+        StringBuffer s2 = new StringBuffer();
+        char ch;
+        int count = 0;
+        for (int i=0; i<s1.length(); i++){
+            ch = s1.charAt(i);
+            count = 1;
+            while (i+1<s1.length() && s1.charAt(i+1) == ch){
+                i++;
+                count++;
+            }
+            s2.append(count).append(ch);
+        }
+        return s2.toString();
+    }
 }
