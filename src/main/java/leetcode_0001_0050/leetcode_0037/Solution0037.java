243a2de2230248f15b99d674fd37fb89ba6dd0d9
sunguanjun <hzsunguanjun.corp.netease.com>
2018-09-24 12:22:27 +0800

    add:解数独

--- src/main/java/leetcode_0001_0050/leetcode_0037/Solution0037.java
+++ src/main/java/leetcode_0001_0050/leetcode_0037/Solution0037.java
@@ -2,6 +2,72 @@ package leetcode_0001_0050.leetcode_0037;
 
 public class Solution0037 {
     public void solveSudoku(char[][] board) {
+        solve(board, 0,0);
+    }
 
+    public boolean solve(char[][] board, int x, int y){
+        if (x == 9 || y==9){
+            return true;
+        }
+        if (board[x][y] == '.'){
+            for (int i=0; i<9; i++){
+                if (isValid(board, x, y, (char)('0'+i))){
+                    board[x][y] = (char)('0'+i);
+                    if (y < 8 ){
+                        if (solve(board, x, y+1))
+                            return true;
+                    }
+                    else {
+                        if (solve(board, x+1, 0))
+                            return true;
+                    }
+                    board[x][y] = '.';
+                }
+            }
+            return false;
+        }
+        else {
+            if (y < 8){
+                return solve(board, x, y+1);
+            }
+            else {
+                return solve(board, x+1, 0);
+            }
+        }
+    }
+
+    public boolean solveSudokuDFS(char[][] board, int i, int j) {
+        if (i == 9) return true;
+        if (j >= 9) return solveSudokuDFS(board, i + 1, 0);
+        if (board[i][j] == '.') {
+            for (int k = 1; k <= 9; ++k) {
+                if (isValid(board, i , j, (char)(k + '0'))) {
+                    board[i][j] = (char)(k + '0');
+                    if (solveSudokuDFS(board, i, j + 1)) return true;
+                }
+                board[i][j] = '.';
+            }
+        } else {
+            return solveSudokuDFS(board, i, j + 1);
+        }
+        return false;
+    }
+
+    public boolean isValid(char[][] board, int x, int y, char target){
+        int x1, y1;
+        for (int i=0; i<9; i++){
+            if (board[x][i] == target){
+                return false;
+            }
+            if (board[i][y] == target){
+                return false;
+            }
+            x1 = x / 3 * 3 + i / 3;
+            y1 = y / 3 * 3 + i % 3;
+            if (board[x1][y1] == target){
+                return false;
+            }
+        }
+        return true;
     }
 }
