659227bdfae60e42648f0a4ad6ba8a157efa9f78
sunguanjun <hzsunguanjun.corp.netease.com>
2018-09-24 10:51:32 +0800

    add:xx

--- src/main/java/leetcode_0001_0050/leetcode_0039/Solution0039.java
+++ src/main/java/leetcode_0001_0050/leetcode_0039/Solution0039.java
@@ -1,5 +1,10 @@
 package leetcode_0001_0050.leetcode_0039;
 
-public class Solution0039 {
+import java.util.List;
 
+public class Solution0039 {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+
+        return null;
+    }
 }
