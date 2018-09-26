8a23abf67c05414e367162e98b2f719158e7610e
sunguanjun <1532945407@qq.com>
2018-09-26 23:29:39 +0800

    add:接雨水

--- src/main/java/leetcode_0001_0050/leetcode_0042/Solution0042.java
+++ src/main/java/leetcode_0001_0050/leetcode_0042/Solution0042.java
@@ -1,5 +1,39 @@
 package leetcode_0001_0050.leetcode_0042;
 
 public class Solution0042 {
+    public int trap(int[] height) {
+        if (height.length <= 2){
+            return 0;
+        }
 
+        int heighest=0, hIndex=0;
+        for (int i=0; i<height.length; i++){
+            if (height[i] > heighest){
+                heighest = height[i];
+                hIndex = i;
+            }
+        }
+
+        int vloume = 0, h1=height[0];
+        for (int i=1; i<hIndex; i++){
+            if (h1 > height[i]){
+                vloume += h1 - height[i];
+            }
+            else {
+                h1 = height[i];
+            }
+        }
+
+        h1=height[height.length-1];
+        for (int i=height.length-1; i>hIndex; i--){
+            if (h1 > height[i]){
+                vloume += h1 - height[i];
+            }
+            else {
+                h1 = height[i];
+            }
+        }
+
+        return vloume;
+    }
 }
--- src/test/java/leetcode_0001_0050/leetcode_0042/Solution0042Test.java
+++ src/test/java/leetcode_0001_0050/leetcode_0042/Solution0042Test.java
@@ -1,5 +1,6 @@
 package leetcode_0001_0050.leetcode_0042;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class Solution0042Test {
@@ -7,6 +8,7 @@ public class Solution0042Test {
 
 	@Test
 	public void test1(){
-
+		int[] nums = {0,1,0,2,1,0,1,3,2,1,2,1};
+		Assert.assertEquals(6, solution0042.trap(nums));
 	}
 }
