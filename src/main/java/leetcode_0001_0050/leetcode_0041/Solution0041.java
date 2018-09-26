d230c7f69e85d555eff8403eb0372f50bef6b95e
sunguanjun <1532945407@qq.com>
2018-09-26 22:26:50 +0800

    add:缺失的第一个正整数

--- src/main/java/leetcode_0001_0050/leetcode_0041/Solution0041.java
+++ src/main/java/leetcode_0001_0050/leetcode_0041/Solution0041.java
@@ -1,5 +1,24 @@
 package leetcode_0001_0050.leetcode_0041;
 
 public class Solution0041 {
+    public int firstMissingPositive(int[] nums) {
+        for (int i=0; i<nums.length ; i++){
+            while (nums[i] > 0 && nums[i] <= nums.length && nums[nums[i]-1] != nums[i]){
+                swap(nums, nums[i]-1, i);
+            }
+        }
 
+        for (int i=0; i<nums.length; i++){
+            if (nums[i] != i+1){
+                return i+1;
+            }
+        }
+        return nums.length+1;
+    }
+
+    public void swap(int[] nums, int a, int b){
+        int tmp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = tmp;
+    }
 }
--- src/test/java/leetcode_0001_0050/leetcode_0041/Solution0041Test.java
+++ src/test/java/leetcode_0001_0050/leetcode_0041/Solution0041Test.java
@@ -1,12 +1,40 @@
 package leetcode_0001_0050.leetcode_0041;
 
+import org.junit.Assert;
 import org.junit.Test;
 
+/**
+ * 示例 1:
+
+ 输入: [1,2,0]
+ 输出: 3
+ 示例 2:
+
+ 输入: [3,4,-1,1]
+ 输出: 2
+ 示例 3:
+
+ 输入: [7,8,9,11,12]
+ 输出: 1
+ */
 public class Solution0041Test {
 	Solution0041 solution0041 = new Solution0041();
 
 	@Test
 	public void test1(){
+		int[] nums = {1,2,0};
+		Assert.assertEquals(3, solution0041.firstMissingPositive(nums));
+	}
 
+	@Test
+	public void test2(){
+		int[] nums = {3,4,-1,1};
+		Assert.assertEquals(2, solution0041.firstMissingPositive(nums));
+	}
+
+	@Test
+	public void test3(){
+		int[] nums = {7,8,9,11,12};
+		Assert.assertEquals(1, solution0041.firstMissingPositive(nums));
 	}
 }
