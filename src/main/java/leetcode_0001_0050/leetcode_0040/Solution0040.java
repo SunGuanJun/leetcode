84009c6b3c19c950b44c7ea776f4afe2f2182d8e
sunguanjun <1532945407@qq.com>
2018-09-26 20:08:55 +0800

    add:组合总和2

--- src/main/java/leetcode_0001_0050/leetcode_0040/Solution0040.java
+++ src/main/java/leetcode_0001_0050/leetcode_0040/Solution0040.java
@@ -1,5 +1,29 @@
 package leetcode_0001_0050.leetcode_0040;
 
-public class Solution0040 {
+import java.util.*;
 
+public class Solution0040 {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        Set<List<Integer>> result = new HashSet<>();
+        Arrays.sort(candidates);
+        combine(candidates, target, 0, 0, new ArrayList<>(), result);
+        return new ArrayList<>(result);
+    }
+
+    public void combine(int[] nums, int target, int sum, int start, List<Integer> tmpList, Set<List<Integer>> result){
+        if (start == nums.length){
+            return;
+        }
+        combine(nums, target, sum, start+1, new ArrayList<>(tmpList), result);
+
+        sum += nums[start];
+        tmpList.add(nums[start]);
+
+        if (target == sum){
+            result.add(tmpList);
+        }
+        else if (target > sum){
+            combine(nums, target, sum, start+1, tmpList, result);
+        }
+    }
 }
--- src/test/java/leetcode_0001_0050/leetcode_0040/Solution0040Test.java
+++ src/test/java/leetcode_0001_0050/leetcode_0040/Solution0040Test.java
@@ -1,12 +1,40 @@
 package leetcode_0001_0050.leetcode_0040;
 
+import com.alibaba.fastjson.JSON;
 import org.junit.Test;
 
 public class Solution0040Test {
 	Solution0040 solution0040 = new Solution0040();
 
+	/**
+	 * 示例 1:
+	 *
+	 *     输入: candidates = [10,1,2,7,6,1,5], target = 8,
+	 *     所求解集为:
+	 *     [
+	 *       [1, 7],
+	 *       [1, 2, 5],
+	 *       [2, 6],
+	 *       [1, 1, 6]
+	 *     ]
+	 * 示例 2:
+	 *
+	 *     输入: candidates = [2,5,2,1,2], target = 5,
+	 *     所求解集为:
+	 *     [
+	 *       [1,2,2],
+	 *       [5]
+	 *     ]
+	 */
 	@Test
 	public void test1(){
+		int[] nums = {10,1,2,7,6,1,5};
+		System.out.println(JSON.toJSONString(solution0040.combinationSum(nums, 8)));
+	}
 
+	@Test
+	public void test2(){
+		int[] nums = {2,5,2,1,2};
+		System.out.println(JSON.toJSONString(solution0040.combinationSum(nums, 5)));
 	}
 }
