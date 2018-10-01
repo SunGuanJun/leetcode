474782422411ec9608cba5bbc74c2c26e39315b3
sunguanjun <1532945407@qq.com>
2018-10-01 18:52:55 +0800

    add:¨排列

--- src/main/java/leetcode_0001_0050/leetcode_0046/Solution0046.java
+++ src/main/java/leetcode_0001_0050/leetcode_0046/Solution0046.java
@@ -1,9 +1,38 @@
 package leetcode_0001_0050.leetcode_0046;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Solution0046 {
     public List<List<Integer>> permute(int[] nums) {
-        return null;
+        List<List<Integer>> result = new ArrayList<>();
+        permute(nums, 0, result);
+        return result;
+    }
+
+    void permute(int[] nums, int start, List<List<Integer>> result){
+        if (start == nums.length-1){
+            result.add(copy(nums));
+            return;
+        }
+        for (int i=start; i<nums.length; i++){
+            swap(nums, start, i);
+            permute(nums, start+1, result);
+            swap(nums, start, i);
+        }
+    }
+
+    List<Integer> copy (int[] nums){
+        List<Integer> result = new ArrayList<>(nums.length);
+        for (int i=0; i<nums.length; i++){
+            result.add(nums[i]);
+        }
+        return result;
+    }
+
+    void swap (int[] nums, int a, int b){
+        int tmp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = tmp;
     }
 }
