bc536f2a0e16ab1d1792043f086c9c21d0fc7443
sunguanjun <1532945407@qq.com>
2019-02-09 15:43:40 +0800

    add:49

--- src/main/java/leetcode_0001_0050/leetcode_0049/Solution0049.java
+++ src/main/java/leetcode_0001_0050/leetcode_0049/Solution0049.java
@@ -1,9 +1,19 @@
 package leetcode_0001_0050.leetcode_0049;
 
-import java.util.List;
+import java.util.*;
 
 public class Solution0049 {
     public List<List<String>> groupAnagrams(String[] strs) {
-        return null;
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs){
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String key = String.valueOf(chars);
+            if (map.get(key) == null){
+                map.put(key, new ArrayList<>());
+            }
+            map.get(key).add(str);
+        }
+        return new ArrayList<>(map.values());
     }
 }
